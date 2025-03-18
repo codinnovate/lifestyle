95755f444fab47968a12794f5bdff74f558c3092
samuel <adeyemis710@gmail.com>
2025-03-18 18:10:13 +0100

    skilled

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero(){
         <div className="flex flex-col mb-10">
             <Logo />
             <div className="mt-20 flex flex-col gap-5 ">
-             <h1 className="text-white !my-10 text-center text-9xl pl-2">
+             <h1 className="text-white !my-10 text-center text-7xl md:text-9xl pl-2">
                 OLA LIFESTYLE
               </h1>
             
--- components/HeroComp.tsx
+++ components/HeroComp.tsx
@@ -5,7 +5,7 @@ export default function HeroComp({text}:{text:string}){
         <div className="flex flex-col mb-10">
             <Logo />
             <div className="mt-20 flex flex-col gap-5 ">
-             <h1 className="text-white !my-10 text-center uppercase text-4xl md:text-9xl pl-2"> 
+             <h1 className="text-white !my-10 text-center uppercase text-7xl md:text-9xl pl-2"> 
                  {text}
              </h1>
             </div>
